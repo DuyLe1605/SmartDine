f2f4f6a0b85a0bcc74dd240c6eca3be382f4c1e8
DuyLe1605 <leminhduy160505@gmail.com>
2025-06-01 09:06:00 +0700

    Tạo refresh token route handler

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,5 +1,11 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType, LogoutBodyType } from "@/schemaValidations/auth.schema";
+import {
+    LoginBodyType,
+    LoginResType,
+    LogoutBodyType,
+    RefreshTokenBodyType,
+    RefreshTokenResType,
+} from "@/schemaValidations/auth.schema";
 import { MessageResType } from "@/schemaValidations/common.schema";
 
 const authApiRequest = {
@@ -15,6 +21,8 @@ const authApiRequest = {
                 headers: { Authorization: `Bearer ${body.accessToken}` },
             }
         ),
+    refreshToken: () => http.post<RefreshTokenResType>("/api/auth/refresh-token", null, { baseUrl: "" }),
+    serverRefreshToken: (body: RefreshTokenBodyType) => http.post<RefreshTokenResType>("/auth/refresh-token", body),
 };
 
 export default authApiRequest;
