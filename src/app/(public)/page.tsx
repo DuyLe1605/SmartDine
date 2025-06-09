29835514510bac719344f84453ad560eb778a107
DuyLe1605 <leminhduy160505@gmail.com>
2025-06-09 21:56:23 +0700

    Fetch danh sách món ăn ở trang chủ

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,19 @@
+import dishApiRequest from "@/apiRequests/dish";
+import { formatCurrency } from "@/lib/utils";
+import { DishListResType } from "@/schemaValidations/dish.schema";
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Home() {
+export default async function Home() {
+    let dishList: DishListResType["data"] = [];
+    try {
+        const result = await dishApiRequest.list();
+        const { data } = result.payload;
+        dishList = data;
+    } catch (error: any) {
+        console.error(error.message ?? "Có lỗi xảy ra");
+    }
+
     return (
         <div className="w-full space-y-4">
             <div className="relative">
@@ -13,9 +26,9 @@ export default function Home() {
                     alt="Banner"
                     className="absolute top-0 left-0 w-full h-full object-cover"
                 />
-                <div className="z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20 text-background dark:text-foreground">
-                    <h1 className="text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold ">
-                        Nhà hàng SmartDine
+                <div className="z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20">
+                    <h1 className="text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold">
+                        Nhà hàng Big Boy
                     </h1>
                     <p className="text-center text-sm sm:text-base mt-4">Vị ngon, trọn khoảnh khắc</p>
                 </div>
@@ -23,23 +36,38 @@ export default function Home() {
             <section className="space-y-10 py-16">
                 <h2 className="text-center text-2xl font-bold">Đa dạng các món ăn</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-                    {Array(4)
-                        .fill(0)
-                        .map((_, index) => (
+                    {dishList &&
+                        dishList.map((item, index) => (
                             <div className="flex gap-4 w" key={index}>
                                 <div className="flex-shrink-0">
-                                    <img
-                                        src="https://ik.imagekit.io/freeflo/production/6b91c700-92c4-4601-8e96-37d84ac3c28c.png?tr=w-2048,q-75&alt=media&pr-true"
-                                        className="object-cover w-[150px] h-[150px] rounded-md"
+                                    <Image
+                                        src={item.image}
+                                        alt={item.name}
+                                        width={150}
+                                        height={150}
+                                        className="object-cover rounded-md"
                                     />
                                 </div>
                                 <div className="space-y-1">
-                                    <h3 className="text-xl font-semibold">Bánh mì</h3>
-                                    <p className="">Bánh mì sandwidch</p>
-                                    <p className="font-semibold">123,123đ</p>
+                                    <h3 className="text-xl font-semibold">{item.name}</h3>
+                                    <p className="">{item.description}</p>
+                                    <p className="font-semibold">{formatCurrency(item.price)}</p>
                                 </div>
                             </div>
                         ))}
+                    {!dishList &&
+                        Array(4)
+                            .fill(0)
+                            .map((_, index) => (
+                                <div className="flex gap-4 w" key={index}>
+                                    <Skeleton className="w-[150px] h-[150px] rounded-md"></Skeleton>
+                                    <div className="space-y-2">
+                                        <Skeleton className="h-5 w-[150px]" />
+                                        <Skeleton className="h-4 w-[200px] mt-5" />
+                                        <Skeleton className="h-4 w-[170px]" />
+                                    </div>
+                                </div>
+                            ))}
                 </div>
             </section>
         </div>
