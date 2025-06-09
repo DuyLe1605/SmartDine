ca4bc11a80df7257dcbfe71be297836e5aaa5aff
DuyLe1605 <leminhduy160505@gmail.com>
2025-06-09 17:19:46 +0700

    Custom Qr Code Canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -8,16 +8,35 @@ interface Props {
     width?: number;
 }
 
-export default function QrCodeTable({ tableNumber, token, width }: Props) {
+export default function QrCodeTable({ tableNumber, token, width = 250 }: Props) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        const tableLink = getTableLink({ token, tableNumber });
+        // Hiện tại ý tưởng là cho thư viện tạo ra 1 canvas ảo,
+        // xong rồi sẽ thêm canvas ảo đó vào Canvas thật của mình
+        // Canvas thật bao gồm chữ và QR Code
 
-        QRCode.toCanvas(canvas, tableLink, function (error) {
+        const canvas = canvasRef.current!;
+        canvas.height = width + 70;
+        canvas.width = width;
+
+        const canvasContext = canvas.getContext("2d")!;
+        canvasContext.fillStyle = "#fff";
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+        canvasContext.font = "20px Arial";
+        // ĐĐiểm giữa dòng chữ là điểm bắt đầu của dòng chữ
+        canvasContext.textAlign = "center";
+        canvasContext.fillStyle = "#000";
+        // canvas.width / 2 để bắT đầu từ giữa
+        canvasContext.fillText(`Bàn số ${tableNumber}`, canvas.width / 2, canvas.width + 20);
+        canvasContext.fillText(`Quét mã Qr để gọi món`, canvas.width / 2, canvas.width + 50);
+
+        const tableLink = getTableLink({ token, tableNumber });
+        const virtualCanvas = document.createElement("canvas");
+        QRCode.toCanvas(virtualCanvas, tableLink, function (error) {
             if (error) console.error(error);
             console.log("success!");
+            canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
         });
     }, []);
     return <canvas ref={canvasRef}></canvas>;
